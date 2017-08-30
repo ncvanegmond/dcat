97401e440a599ead48336cef2c80794254c02ad5
ncvanegmond <ncvanegmond@gmail.com>
2017-08-30 12:04:17 +0200

    heroku deployment

--- config/settings/base.py
+++ config/settings/base.py
@@ -120,8 +120,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Imported these setting from devcenter.Heroku
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dcai_data',
+        'USER': 'dcai_classification',
+        'PASSWORD': '',
+        # remove localhost from hosts
+        'HOST': '',
+        'PORT': '',
     }
 }
 
